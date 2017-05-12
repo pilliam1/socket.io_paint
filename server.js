9b816ce621c4e2ed12bf4df2f6323f4214b0b7f2
William Macleod <willrmacleod@gmail.com>
2017-05-12 22:21:41 +0800

    working app

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 var express = require('express');
 var app = express();
 
+var server = app.listen(3000);
+app.use(express.static('public'));
+
 console.log("hello fellas");
+
+socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+//taking in a socket because that is what becomes of it when a newConnection is made.
+function newConnection(socket){
+    console.log('new connection: ' + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+//recieves messages and sends them back out immediately
+    function mouseMsg(data){
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+}
